b75de13858243ca556d8295f5b70e82492ba9572
Mads Dam Hvidbjerg Eckardt <maeck@eg.dk>
2022-08-03 11:50:04 +0200

    More

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 5555,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {'^/api': ''},
+        secure: false,
+        logLevel: 'debug'
+      },
+    },
+  },
 })
